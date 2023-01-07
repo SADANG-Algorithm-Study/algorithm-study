1e3c023a2880d44b9cc1392ed640b6a18b3fb769
ju-seong-hyeon <77434193+ju-seong-hyeon@users.noreply.github.com>
2023-01-07 19:35:42 +0900

    Delete 주성현 20230107 directory

--- 주성현 20230107/1.py
+++
@@ -1,9 +0,0 @@
-def solution(numbers, k):
-    answer = numbers[0]
-    n = len(numbers)
-    for i in range(k - 1):
-        answer = answer + 2
-        if (answer > numbers[n - 1]):
-            answer -= numbers[n - 1]
-
-    return answer
